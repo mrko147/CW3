9105c61bc980e54854c2cea663c0245735a42d76
mrko147 <95499236+mrko147@users.noreply.github.com>
2023-03-01 12:40:55 +0000

    build the basic packages

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+import Test from './components/test.vue';
+import TheWelcome from './components/test.vue'
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      
+
+   
+    </header>
+
+    <main>
+      <Test> </Test>
+    </main>
+  </div>
+</template>
+
+
---
+++ src/components/test.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+
+
+<template>
+<div class="greetings">
+    <h1 >hello world</h1>
+</div>
+</template>
